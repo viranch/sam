c5e75d86a4dad8659841b9b2a44854961fe28248
viranch <viranch.mehta@gmail.com>
2010-10-19 00:03:51 +0530

    Minor change in install script.

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p $HOME/.sam
 cp ./src/*py* $HOME/.sam/
+chmod a+x $HOME/.sam/sam.pyw
 echo "Please Enter root password"
-sudo cp sam1 /usr/bin/
+sudo cp sam /usr/bin/
 echo "Successful."
