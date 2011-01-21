ace91fcf4f8e51a474cf810c6eecc2615cc7c226
viranch <viranch.mehta@gmail.com>
2011-01-22 01:06:24 +0530

    Systematic+Automated installation, see README

--- setup.py
+++ setup.py
@@ -2,13 +2,22 @@
 
 from distutils.core import setup
 
-setup(
-	name='SAM',
-	version='1.0',
-	description='Cyberoam Account Manager',
-	author='Viranch Mehta / Mohit Kothari',
-	author_email='viranch.mehta@gmail.com / mohitrajkothari@gmail.com',
-	url='http://www.butbucket.org/viranch/sam',
-	packages=['sam'],
-	scripts=['scripts/sam'],
-     )
+def do():
+	setup(
+		name='SAM',
+		version='1.0',
+		description='Cyberoam Account Manager',
+		author='Viranch Mehta / Mohit Kothari',
+		author_email='viranch.mehta@gmail.com / mohitrajkothari@gmail.com',
+		url='http://www.butbucket.org/viranch/sam',
+		packages=['sam'],
+		scripts=['scripts/sam'],
+	)
+
+import sys
+
+sys.argv.append ('build')
+do()
+sys.argv[1] = 'install'
+do()
+
